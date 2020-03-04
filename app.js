15e78fafc391143e653daa616627e8798c0c04d0
Joshua Lokken <jrrlokken@gmail.com>
2020-03-03 19:18:01 -0600

    Initial commit

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-// Check off specific todos by clicking
-$("ul").on("click", "li", function() {
-	$(this).toggleClass("completed");
-});
+const results = document.getElementById('results');
+const lis = document.querySelectorAll('li');
+const icons = document.querySelectorAll('li i');
 
-$("ul").on("click", "span", function(event) {
-	$(this).parent().fadeOut(500, function() {
-		$(this).remove();
+// Strikethrough to show completed tasks
+for (let li of lis) {
+	li.addEventListener('click', function() {
+		li.classList.toggle('completed');
 	});
-	event.stopPropagation();
-});
+}
 
-$("input[type='text']").keypress(function(event) {
-	if(event.which === 13) {
-		var todoText = $(this).val();
-		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>");
-		$(this).val('');
+// Allow clicking of the trash icon to remove item
+for (let icon of icons) {
+	icon.addEventListener('click', function() {
+		icon.parentElement.parentElement.remove();
+	});
+}
+
+const input = document.querySelector('input[type="text"]');
+const ul = document.querySelector('ul');
+
+input.addEventListener('keypress', function(event) {
+	if (event.keyCode === 13) {
+		let newTodo = document.createElement('li');
+		let newSpan = document.createElement('span');
+		let newIcon = document.createElement('i');
+		ul.append(newTodo);
+		textContent = input.value;
+		newTodo.append(newSpan);
+		newSpan.append(newIcon);
+		newIcon.classList.add('fa', 'fa-trash');
+		newTodo.append(textContent);
+		input.value = '';
 	}
 });
 
-$("#toggle-form").click(function(){
-	$("input[type='text']").fadeToggle();
-});
+// Still need event listeners for newly added elements
+// Then, add local storage
