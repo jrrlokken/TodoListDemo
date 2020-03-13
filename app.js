c783e6e4c180ddc3f3656bd0da1fac64bc51b994
Joshua Lokken <jrrlokken@gmail.com>
2020-03-13 15:33:56 -0500

    TodoList completed

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const results = document.getElementById('results');
 const form = document.querySelector('form');
 const input = document.querySelector('input[type="text"]');
-const todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : []
+// const todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : []
+const todos = [];
 
 const todoMaker = text => {
   const newTodo = document.createElement('li');
@@ -11,7 +12,6 @@ const todoMaker = text => {
   newTodo.append(text);
   results.append(newTodo);
   todos.push(text);
-  console.log(todos);
 }
 
 todos.forEach(todo => {
@@ -20,8 +20,9 @@ todos.forEach(todo => {
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  todoMaker(input.value);
-  localStorage.setItem("todos", JSON.stringify(todos));
+  let todoText = input.value;
+  todoMaker(todoText);
+  // localStorage.setItem("todos", JSON.stringify(todos));
   form.reset();
 });
 
@@ -29,9 +30,7 @@ results.addEventListener('click', function() {
 	if (event.target.tagName === 'LI') {
 		event.target.classList.toggle('completed');
 	} else if (event.target.tagName === 'SPAN') {
-    const todo = event.target.parentElement.textContent;
-    console.log(todo);
-    // event.target.parentElement.remove();
-
+    // const todo = event.target.parentElement.textContent;
+    event.target.parentElement.remove();
   }
 });
